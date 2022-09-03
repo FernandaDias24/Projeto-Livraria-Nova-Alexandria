6dc94497f187021cf0b942d7f120337b13fe8dd3
devlucas <lucasalbino@gmail.com>
2022-09-02 22:39:25 -0300

    Atualização da Pasta Model

--- models/modCadlivros.js
+++ models/modCadlivros.js
@@ -17,25 +17,55 @@ class Livro{
             return null
         }
     }
-}
 
-constructor( editora, titulo, autor, genero, ISBN )
-{
-    this.editora   = editora
-    this.titulo    = titulo
-    this.autor     = autor
-    this.genero    = genero
-    this.isbn      = ISBN
-    this.id        = Livro.proximoID++
-}
-
-dados()
-{
-    return {
-        "editora": this.editora,
-        "titulo": this.titulo,
-        "autor": this.autor,
-        "genero": this.genero,
-        "ISBN": this.ISBN,
+    constructor( editora, titulo, autor, genero, ISBN )
+    {
+        this.editora   = editora
+        this.titulo    = titulo
+        this.autor     = autor
+        this.genero    = genero
+        this.isbn      = ISBN
+        this.id        = Livro.proximoID++
     }
+
+    dados()
+    {
+        return {
+            "editora": this.editora,
+            "titulo": this.titulo,
+            "autor": this.autor,
+            "genero": this.genero,
+            "ISBN": this.ISBN,
+        }
+    }
+
+    save()
+    {
+        const livro = Livro.findById( this.id )
+
+        if ( livro === null ) {
+            livrosEstoque.all.push( this )
+        }
+    }
+
+    update( novasInformacoes )
+    {
+        // novasInformações => objeto
+        for ( let campo in novasInformacoes )
+        {
+            // Importante para o funcionamento do projeto
+            if ( typeof this[campo] !== 'function' )
+                this[campo] = novasInformacoes[campo]
+        }
+    }
+
+    delete()
+    {
+        const posicao = livrosEstoque.all.findIndex( lvr => lvr.id === id )
+        if ( posicao >= 0 )
+        livrosEstoque.all.splice( posicao, 1 )
+    }
+
 }
+
+
