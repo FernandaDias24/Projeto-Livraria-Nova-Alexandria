67f53a7896d96c0ed1dde17fd511a145af2f9881
alana-as <abs_alana@hotmail.com>
2022-09-03 00:02:26 -0300

    Atualização Testada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import estoqueLivros from './controllers/estoqueLivros.js';
 const app = express();
-const porta = process.env.PORT || 3000;
+const porta = process.env.PORT || 3001;
 app.use(express.json());
 estoqueLivros(app);
 
